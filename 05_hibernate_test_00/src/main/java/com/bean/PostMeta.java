4bce68415984fa6bdf76b0ad9c1390adb9eba16d
LinHJ <linhognjie@ffcs.cn>
2019-03-03 21:35:13 +0800

    新增Hibernate模块

---
+++ 05_hibernate_test_00/src/main/java/com/bean/PostMeta.java
@@ -0,0 +1,41 @@
+package com.bean;
+
+public class PostMeta {
+
+    private String metaId;
+    private String postId;
+    private String metaKey;
+    private String metaValue;
+
+    public String getMetaId() {
+        return metaId;
+    }
+
+    public String getMetaKey() {
+        return metaKey;
+    }
+
+    public String getMetaValue() {
+        return metaValue;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setMetaId(String metaId) {
+        this.metaId = metaId;
+    }
+
+    public void setMetaKey(String metaKey) {
+        this.metaKey = metaKey;
+    }
+
+    public void setMetaValue(String metaValue) {
+        this.metaValue = metaValue;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+}
